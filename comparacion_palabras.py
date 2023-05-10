faa60f4511b8a2eb089af1ff4ab12bef1841a24c
pandasaurus07 <114919763+pandasaurus07@users.noreply.github.com>
2023-05-10 13:27:54 +0200

    Create comparacion_palabras.py

---
+++ comparacion_palabras.py
@@ -0,0 +1,8 @@
+def palabras(palabra1, palabra2)
+  if palabra1 == palabra2:
+      return True
+  else:
+     return False
+  palabra1 = (input("Dime una palabra: "))
+  palabra2 = (input("Dime una palabra: "
+  print = (palabras(palabra1, palabra2))
